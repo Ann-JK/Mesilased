1dc4f640b90c0986ca2223f28a25f426e179fd5c
Marcus <marcus.villems@gmail.com>
2023-12-28 19:48:29 +0200

    init

---
+++ Mesilased/Models/Bees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mesilased.Models
+{
+    public class Bees
+    {
+            public string Name { get; set; }
+            public string ScientificName { get; set; }
+            public string Description { get; set; }
+            public List<string> Images { get; set; }
+    }
+}
