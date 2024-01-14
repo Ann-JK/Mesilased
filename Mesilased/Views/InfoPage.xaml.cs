2a65bbc3789ce456debeb8abf1ebe4e026f9d541
Ann-JK <annabelkongas@gmail.com>
2024-01-14 21:34:11 +0200

    redo broken local branch changes into this one

---
+++ Mesilased/Views/InfoPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace Mesilased.Views;
+
+public partial class InfoPage : ContentPage
+{
+	public InfoPage()
+	{
+		InitializeComponent();
+	}
+
+    async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+    {
+
+    }
+
+    async void Patreon_Clicked(System.Object sender, System.EventArgs e)
+    {
+        Launcher.OpenAsync(new Uri("http://www.patreon.com"));
+    }
+}
