86e14e65e69b9f9a82029ab52a67eff827d2e387
viljar norman <99358447+viljarnorman@users.noreply.github.com>
2024-01-17 15:17:25 +0200

    BeeSpecimen

---
+++ Mesilased/Models/BeeSpecimen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mesilased.Models
+{
+    public class BeeSpecimen
+    {
+        public string Name { get; set; }
+
+        public string HeroImage { get; set; }
+        public string Description { get; set; }
+
+        public string Icon { get; set;}
+
+        public List<string> Images { get; set; }
+
+        public Brush Backround { get; set; }
+    }
+}
